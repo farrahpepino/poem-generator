9caf74fd303504df12ba8268e53b2f7f7b468b4a
fpepino1 <115742428+fpepino1@users.noreply.github.com>
2023-12-03 18:30:22 -0500

    APi integration and css enhancement

--- src/poem-generator-page.js
+++ src/poem-generator-page.js
@@ -1,24 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   function displayPoem(response) {
     console.log("poem generated");
+
+    new Typewriter("#verse", {
+      strings: response.data.answer,
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
   }
 
   function generatePoem(event) {
     event.preventDefault();
 
-    new Typewriter("#verse", {
-      strings: "La tombe dit à la rose",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
-
+    let instructionsInput = document.querySelector("#instructions-input");
     let key = "010at9ca97fd6359640c0357507fb1co";
-    let prompt = "Generate a poem";
-    let context = "Make sure it is unique";
+    let context =
+      "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in a <div> element and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem.";
+    let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
     console.log("generating poem...");
+    let poemElement = document.querySelector("#verse");
+    poemElement.classList.remove("hidden");
+
+    poemElement.innerHTML = ` <div class="generating"> Generating a poem about ${instructionsInput.value}</div>`;
     axios.get(apiUrl).then(displayPoem);
   }
 
