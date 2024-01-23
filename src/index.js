dc48d6fef544f6ca2f59b206afe37aba925a9df7
FIRE <115742428+fpepino1@users.noreply.github.com>
2024-01-23 00:34:19 -0500

    changed landing page

--- src/index.js
+++
@@ -1,9 +0,0 @@
-let joinElement = document.querySelector("#join-button");
-joinElement.addEventListener("click", function () {
-  window.location.href = "src/poem-generator-page.html";
-});
-
-let logoElement = document.querySelector("#logo-button");
-logoElement.addEventListener("click", function () {
-  window.location.href = "../index.html";
-});
